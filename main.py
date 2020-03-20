7269c4c0a2ac63986ecf1ac141b3ac115667ec82
ryanrych <ryanjrychlak@gmail.com>
2020-03-20 11:17:26 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#kamchatka = @2125071
+#rangoon = @1298822
+#kiev = @703446
+africanCountries=["algeria","egypt","morocco","zimbabwe","south-africa","liberia","kenya"]
+asianCountries=["kazakhstan","jordan","iraq","azerbaijan","thailand","china","sri-lanka","bangladesh","palestine","russia","indonesia","Israel","afghanistan","pakistan","nepal","india","malaysia","oman","saudi-arabia","south-korea","singapore","taiwan","uzbekistan","georgia","iran","japan","mongolia","armenia"]
+europeanCountries=["netherlands","greece","serbia","germany","slovakia","belgium","romania","hungary","denmark","ireland","finland","turkey","portugal","uk","spain","belarus","russia","france"]
