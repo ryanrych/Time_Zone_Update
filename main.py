bfb9a3e4dcc0e83d8827348d93e58b3da06a65b9
ryanrych <ryanjrychlak@gmail.com>
2020-03-28 19:07:26 -0500

    Added algorithm to update the start DST data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from urllib import *
+from calendar import *
+from datetime import *
 
 countries={"Algiers":"algeria","Cairo":"egypt","Casablanca":"morocco","Harare":"zimbabwe","Johannesburg":"south-africa","Monrovia":"liberia","Nairobi":"kenya","Hong-Kong":"hong-kong","Almaty":"kazakstan","Amman":"jordan","Baghdad":"iraq","Baku":"azerbaijan","Bangkok":"thailand","Chongqing":"china","Colombo":"sri-lanka","Dhaka":"bangladesh","Hebron":"palestine","Irkutsk":"russia","Jakarta":"indonesia","Jerusalem":"israel","Kabul":"afghanistan","Karachi":"pakistan","Kathmandu":"nepal","Kolkata":"india","Krasnoyarsk":"russia","Kuala-Lumpur":"malaysia","Magadan":"russia","Muscat":"oman","Novosibirsk":"russia","Riyadh":"saudi-arabia","Seoul":"south-korea","Singapore":"singapore","Shanghai":"china","Taipei":"taiwan","Tashkent":"uzbekistan","Tbilisi":"georgia","Tehran":"iran","Tokyo":"japan","Ulaanbaatar":"mongolia","Urumqi":"china","Vladivostok":"russia","Yakutsk":"russia","Yekaterinburg":"russia","Yerevan":"armenia","Amsterdam":"netherlands","Athens":"greece","Belgrade":"serbia","Berlin":"germany","Bratislava":"slovakia","Brussels":"belgium","Bucharest":"romania","Budapest":"hungary","Copenhagen":"denmark","Dublin":"ireland","Helsinki":"finland","Istanbul":"turkey","Lisbon":"portugal","London":"uk","Madrid":"spain","Ljubljana":"slovenia","Minsk":"belarus","Moscow":"russia","Paris":"france","Prague":"czech-republic","Riga":"latvia","Rome":"italy","Sarajevo":"bosnia-herzegovina","Skopje":"republic-of-macedonia","Sofia":"bulgaria","Stockholm":"sweden","Tallinn":"estonia","Vienna":"austria","Vilnius":"lithuania","Warsaw":"poland","Zagreb":"croatia","Apia":"samoa","Auckland":"new-zealand","Chatham-Islands":"new-zealand","Fakaofo":"tokelau","Honolulu":"usa","Majuro":"marshall-islands","Noumea":"france","Port-Moresby":"papua-new-guinea","Midway":"usa","Pago-Pago":"usa","Guatemala":"guatemala","Anchorage":"usa","Adak":"usa","Buenos-Aires":"argentina","Bogota":"colombia","Boise":"usa","Caracas":"venezuela","Chicago":"usa","Chihuahua":"mexico","Dawson-Creek":"canada","Denver":"usa","Detroit":"usa","Edmonton":"canada","Fort-Nelson":"canada","Glace-Bay":"canada","Happy-Valley-Goose-Bay":"canada","Halifax":"canada","Indianapolis":"usa","Juneau":"usa","La-Paz":"bolivia","Lima":"peru","Los-Angeles":"usa","Mazatlan":"mexico","Monterrey":"mexico","Montevideo":"uruguay","Montreal":"canada","New-York":"usa","Nome":"usa","Phoenix":"usa","Regina":"canada","Santiago":"chile","Sao-Paulo":"brazil","Saint-Johns":"antigua-and-barbuda","Saint-Thomas":"us-virgin","Thunder-Bay":"canada","Tijuana":"Mexico","Mexico-City":"mexico","Vancouver":"canada","Toronto":"canada","Whitehorse":"canada","Winnipeg":"canada"}
 months=["January","February","March","April","May","June","July","August","September","October","November","December"]
@@ -69,6 +71,8 @@ for i in range(len(table)):
 
     page="".join(list(urlopen(url)))
 
+    print city
+
     start=page.find("UTC/GMT")
     if page[start-3:start-1]=="No":
         table[i][1]=0
@@ -86,7 +90,7 @@ for i in range(len(table)):
     page=urlopen(url.replace("zone","change"))
     page="".join(list(page))
     start=page.find("Start DST:")
-    if page.find("Start DST:")==-1:
+    if start==-1:
         table[i][2]="FALSE"
         table[i][3]=0
         for j in range(4,12):
@@ -100,28 +104,23 @@ for i in range(len(table)):
             day+=page[step]
             step+=1
         day=day.split()
-        table[i][4]=months.index(day[2])
-        #add week
+        print day
+        table[i][4]=months.index(day[1])
         table[i][6]=day[0][:-1]
-        start=page.find("Next Change:")
-        start+=168
+        month = monthcalendar(datetime.now().year, table[i][4])
+        for j in range(len(month)):
+            if int(day[2][:-1]) in month[j]:
+                table[i][5]
+                break
+        start=page.find("When local standard time")
+        start=page.find("<strong>",start)
+        start+=8
         step=start
-        hour=""
-        while page[step]!=",":
-            hour+=page[step]
-            step+=1
-        table[i][7]=hour.split()[3]
-
-        start=page.find("End DST:")+33
-        step=start
-        day=""
+        time=""
         while page[step]!="<":
-            day+=page[step]
+            time+=page[step]
             step+=1
-        day=day.split()
-        table[i][8]=months.index(day[2])
-        table[i][10]=day[0][:-1]
-
+        table[i][7]=time
 
 
 for x in table:
